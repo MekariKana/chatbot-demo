b50edf151d0f1084e0141e00a32d0bbeb258593b
MekariKana <20190101km@gmail.com>
2023-01-22 18:06:36 +0900

    🎨 ボタン機能作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import defaultDataset from './dataset';
 import './assets/styles/style.css';
+import {AnswersList} from "./components/index";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,13 +13,25 @@ export default class App extends React.Component {
       dataset: defaultDataset,
       open: false
     }
+    console.log("1");
+  }
+
+  initAnswer = () => {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const initAnswers = initDataset.answers;
+
+    this.setState({answers: initAnswers});
+  }
+
+  componentDidMount(){
+    this.initAnswer();
   }
   
   render() {
     return (
       <section className="c-section">
         <div className="c-box">
-
+          <AnswersList answers={this.state.answers} />
         </div>
       </section>
     )
